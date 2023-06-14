f2153bb766760ded5d2fb79c718ff80555759564
hyoddaeng <won10127@naver.com>
2023-06-14 16:55:05 +0900

    [Feat]

--- final06/src/main/java/kr/co/itwill/member/MemberCont.java
+++ final06/src/main/java/kr/co/itwill/member/MemberCont.java
@@ -1,5 +1,7 @@
 package kr.co.itwill.member;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
