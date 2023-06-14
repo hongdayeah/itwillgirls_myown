61c7a6b10b5898ee35cd284ff1951f2df4e00161
hongdayeah <hong_daye@hanmail.net>
2023-06-14 09:35:09 +0900

    [Fix] Program 부분 에러 수정(삭제)

--- final06/src/main/java/kr/co/itwill/program/ProgramDAO.java
+++ final06/src/main/java/kr/co/itwill/program/ProgramDAO.java
@@ -20,45 +20,4 @@ public class ProgramDAO {
 		System.out.println("-----ProgramDAO() 객체 생성됨");
 	}
 	
-	public List<ProgramDTO> list(String pro_obj){
-		List<ProgramDTO> list = null;
-		try {
-			sql = new StringBuilder();
-			
-			sql.append(" SELECT pro_obj, pro_name, prochar_no, pro_limit, pro_fee, prorec_start, prorec_end, proper_start, proper_end, pro_day, pro_age, pro_exp, pro_poster, pro_img, pro_imgsize, pro_regdate, pro_readcnt FROM program_info ");
-			sql.append(" ORDER BY pro_regdate DESC ");
-			
-			RowMapper<ProgramDTO> rowMapper = new RowMapper<ProgramDTO>() {
-				@Override
-				public ProgramDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
-					ProgramDTO dto = new ProgramDTO();
-					dto.setPro_obj(rs.getString("pro_obj"));
-					dto.setPro_name(rs.getString("pro_name"));
-					dto.setProchar_no(rs.getString("prochar_no"));
-					dto.setPro_limit(rs.getInt("pro_limit"));
-					dto.setPro_fee(rs.getInt("pro_fee"));
-					dto.setProrec_start(rs.getString("prorec_start"));
-					dto.setProrec_end(rs.getString("prorec_end"));
-					dto.setProper_start(rs.getString("proper_start"));
-					dto.setProper_end(rs.getString("proper_end"));
-					dto.setPro_day(rs.getString("pro_day"));
-					dto.setPro_age(rs.getString("pro_age"));
-					dto.setPro_exp(rs.getString("pro_exp"));
-					dto.setPro_poster(rs.getString("pro_poster"));
-					dto.setPro_img(rs.getString("pro_img"));
-					dto.setPro_imgsize(rs.getInt("pro_imgsize"));
-					dto.setPro_regdate(rs.getString("pro_regdate"));
-					dto.setPro_readcnt(rs.getInt("pro_readcnt"));
-					
-					return dto;
-				}//mapRow() end
-			}; //rowMapper end
-			
-			list = jt.query(sql.toString(), rowMapper);
-		}catch(Exception e) {
-			System.out.println("프로그램 목록 조회 실패 : " + e);
-		}
-		return list;
-	}//list() end
-	
 }
--- final06/src/main/webapp/WEB-INF/views/program/list.jsp
+++ final06/src/main/webapp/WEB-INF/views/program/list.jsp
@@ -7,37 +7,7 @@
 
 <div>프로그램 목록</div>
 
-<div class="container">
-	<table class="table table-bordered">
-	<tr>
-		<c:forEach items="${list}" var="row" varStatus="vs">
-			<td>
-				<c:choose>
-					<c:when test="${rows.PRO_IMG != '-'}">
-						<img src="/storage/${row.PRO_IMG}" width="100px">
-					</c:when>
-					<c:otherwise>
-						등록된 사진 없음<br>
-					</c:otherwise>
-				</c:choose>
-				<br>
-				프로그램명 : <a>${row.PRO_NAME}</a>
-				<br>
-				<!-- 설명 -->
-				${row.PRO_EXP}
-				<br>
-				일시 : ${row.PROPER_START}<br>
-				대상 : ${row.PRO_AGE}<br>
-				비용 : ${row.PRO_FEE}<br>
-			</td>
-			<!-- 테이블 한 줄에 3칸씩 -->
-			<c:if test="${vs.count mod 3==0}">
-				</tr><tr>
-			</c:if>
-		</c:forEach>
-	</tr>
-	</table>
-</div>
+
 
 <!-- 본문 끝 -->
         
