13305c14f5badb990db88c269a9a50c2115efbf9
hyoddaeng <won10127@naver.com>
2023-06-14 16:53:41 +0900

    [Feat] 대관안내 추가 및 수정

--- final06/src/main/java/kr/co/itwill/member/MemberCont.java
+++ final06/src/main/java/kr/co/itwill/member/MemberCont.java
@@ -1,7 +1,5 @@
 package kr.co.itwill.member;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
