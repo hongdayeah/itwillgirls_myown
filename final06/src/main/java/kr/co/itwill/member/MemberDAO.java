0260cdf5b6130a8dba7e733205bb8d9dfcacaf0a
hyejiiin <mnonz@naver.com>
2023-06-18 16:00:30 +0900

    [Fix] AWS RDS 삭제 후 재생성 및 연결

--- final06/src/main/java/kr/co/itwill/member/MemberDAO.java
+++ final06/src/main/java/kr/co/itwill/member/MemberDAO.java
@@ -1,41 +1,22 @@
 package kr.co.itwill.member;
 
-import java.util.Map;
+import javax.servlet.http.HttpSession;
 
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
+public interface MemberDAO {
 
-@Repository
-public class MemberDAO {
+	//회원가입
+	public void memberJoin(MemberDTO dto);
 
-	//DBOpen dbopen = new DBOpen()와 동일한 형태
-	//@Autowired 스프링컨테이너가 생성해 준 객체를 연결
-	@Autowired
-	private JdbcTemplate jt;
+	//아이디 중복확인
+	public int idCheck(String p_id);
 
-	StringBuilder sql = null;
-	
-	public MemberDAO() {
-		System.out.println("-----MemberDAO() 객체 생성됨");
-	}//end
-	
-	@Autowired
-	SqlSession sqlSession;
-	
-	public void insert(Map<String, Object> map) {
-		sqlSession.insert("member.memberJoinForm", map);
-	}//insert() end
-	
-	//로그인은 인서트가 아닌 것 같은데 조금 더 고민해 보자~
-	public void login(Map<String, Object> map) {
-		sqlSession.insert("member.memberLoginForm", map);
-	}//login() end
-	
-	public void joinProc(Map<String, Object> map) {
-		sqlSession.insert("member.joinProc", map);
-	}//login() end
-	
-	
-}//class end
+	//로그인 체크
+	public boolean loginCheck(MemberDTO dto);
+
+	//로그인 정보
+	public MemberDTO viewMember(MemberDTO dto);
+
+	//로그아웃
+	public void logout(HttpSession session);
+
+}// interface end
