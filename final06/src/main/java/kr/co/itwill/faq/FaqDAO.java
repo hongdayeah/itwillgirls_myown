1ef6e9762ac0b2fde1206b77c342083ca384615d
hyoddaeng <won10127@naver.com>
2023-06-26 22:42:39 +0900

    [Docs] faq 수정

--- final06/src/main/java/kr/co/itwill/faq/FaqDAO.java
+++ final06/src/main/java/kr/co/itwill/faq/FaqDAO.java
@@ -50,6 +50,33 @@ public class FaqDAO {
 		}//list() end
 	
 	
+	
+	public List<FaqDTO> list2() {
+		List<FaqDTO> list2=null;
+		try {
+			sql=new StringBuilder();
+			sql.append(" SELECT f_no, f_cate, f_question, f_answer ");
+			sql.append(" FROM faq ");
+			
+			RowMapper<FaqDTO> rowMapper=new RowMapper<FaqDTO>() {
+				@Override
+				public FaqDTO mapRow(ResultSet rs, int rowNum) throws SQLException{
+					FaqDTO dto=new FaqDTO();
+					dto.setF_no(rs.getInt("f_no"));
+					dto.setF_cate(rs.getString("f_cate"));
+					dto.setF_question(rs.getString("f_question"));
+					dto.setF_answer(rs.getString("f_answer"));
+					return dto;
+					}
+				};
+				list2=jt.query(sql.toString(), rowMapper);
+			}catch(Exception e) {
+				System.out.println("list 목록실패: " + e);
+			}
+			return list2;
+		}//list2() end
+	
+	
 	public int create(FaqDTO dto) {
 		int cnt=0;
 		try {
@@ -65,4 +92,45 @@ public class FaqDAO {
 		return cnt;
 	}//create end
 	
+	
+	public FaqDTO read(int f_no) {
+		FaqDTO dto=null;
+		try {
+			sql=new StringBuilder();
+			sql.append(" SELECT f_no, f_cate, f_question, f_answer ");
+			sql.append(" FROM faq ");
+			sql.append(" WHERE f_no =" + f_no );
+			
+			RowMapper<FaqDTO> rowMapper=new RowMapper<FaqDTO>() {
+				@Override
+				public FaqDTO mapRow(ResultSet rs, int rowNum) throws SQLException{
+					FaqDTO dto=new FaqDTO();
+					dto.setF_no(rs.getInt("f_no"));
+					dto.setF_cate(rs.getString("f_cate"));
+					dto.setF_question(rs.getString("f_question"));
+					dto.setF_answer(rs.getString("f_answer"));
+					return dto;
+				}
+			};
+			dto=jt.queryForObject(sql.toString(), rowMapper);
+		}catch(Exception e) {
+			System.out.println("상세보기 실패: " + e);
+		}
+		return dto;
+	}//read() end
+	
+	
+	public int delete (int f_no) {
+		int cnt=0;
+		try {
+			sql=new StringBuilder();
+			sql.append(" DELETE FROM faq ");
+			sql.append(" WHERE f_no = ? ");
+			
+			cnt=jt.update(sql.toString(), f_no);
+		}catch(Exception e) {
+			System.out.println("FAQ 삭제실패: " + e);
+		}//end
+		return cnt;
+	}//delete() end
 }//class() end
--- final06/src/main/webapp/WEB-INF/views/faq/faqDelete.jsp
+++ final06/src/main/webapp/WEB-INF/views/faq/faqDelete.jsp
@@ -3,11 +3,11 @@
 
 <%@ include file="../header.jsp" %>
 
-<h3>공연삭제</h3>
-<div class="title"> 공지삭제 </div>
-<form name="not" method="post" action="delete.do">
-	<input type="hidden" name="not_no" value="${requestScope.not_no}">
-	<div class="not_con">
+<h3>FAQ삭제</h3>
+<div class="title"> FAQ삭제 </div>
+<form name="FAQ" method="post" action="delete.do">
+	<input type="hidden" name="f_no" value="${requestScope.f_no}">
+	<div class="f_con">
 		<p>정말로 삭제하시겠습니까?</p>
 	</div>
 	
