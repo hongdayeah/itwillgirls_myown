d3d956ddfe36ff6a84e805db4b5d83263e3a391f
hongdayeah <hong_daye@hanmail.net>
2023-06-27 18:54:49 +0900

    [Feat] hyang test CRUD 구현, home, header에 장바구니 dropdown menu 추가

--- final06/src/main/java/kr/co/itwill/hyangtest/HyangtestCont.java
+++ final06/src/main/java/kr/co/itwill/hyangtest/HyangtestCont.java
@@ -1,11 +1,19 @@
 package kr.co.itwill.hyangtest;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-//관리자가 테스트 관리하는 cont
+import kr.co.itwill.sungtest.SungtestDTO;
+
+//관리자가 테스트 질문지 관리하는 cont
 @Controller
 public class HyangtestCont {
 
@@ -14,12 +22,87 @@ public class HyangtestCont {
 		System.out.println("-----HyangtestCont()객체 생성됨");
 	}
 	
+	@Autowired
+	private HyangtestDAO dao;
+	
+	////////////////////////////////////////////////관리자
 	//관리자가 hlist 볼 수 있는 목록
 	@RequestMapping("/test/hlist.do")
 	public ModelAndView hlist() {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("test/hlist");
+		List<HyangtestDTO> list = dao.list();
+		mav.addObject("hlist", list);
 		
 		return mav;
 	}//hlist() end
+	
+	//질문 추가
+	@RequestMapping(value="/test/hcreate.do", method=RequestMethod.GET)
+	public ModelAndView hcreateForm() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("test/hcreateForm");
+		
+		return mav;
+	}//hcreateForm() end
+	
+	@RequestMapping(value="/test/hcreate.do", method=RequestMethod.POST)
+	public ModelAndView hcreateProc(@ModelAttribute HyangtestDTO dto, HttpServletRequest req) {
+		ModelAndView mav = new ModelAndView();
+		
+		
+		int cnt = dao.create(dto);
+		
+		if(cnt==0) {
+			System.out.println("stest 질문 등록 실패");
+		}
+		mav.setViewName("redirect:/test/hlist.do");
+		return mav;
+	}//hcreateProc() end
+	
+	//질문 수정
+	@RequestMapping(value="/test/hupdate.do", method=RequestMethod.GET)
+	public ModelAndView update(int htest_no) {
+		ModelAndView mav= new ModelAndView();
+		mav.setViewName("test/hupdateForm");
+		HyangtestDTO dto = dao.read(htest_no);
+		mav.addObject("dto", dto);
+		mav.addObject("htest_no", htest_no);
+		
+		return mav;
+	}//update() end
+	
+	@RequestMapping(value="/test/hupdate.do", method=RequestMethod.POST)
+	public ModelAndView updateProc(int htest_no, @ModelAttribute HyangtestDTO dto) {
+		ModelAndView mav = new ModelAndView();
+		
+		//System.out.println(htest_no);
+		
+		int cnt = dao.update(dto);
+		
+		if(cnt==0) {
+			System.out.println("htest 질문 수정 실패");
+		}
+		
+		mav.setViewName("redirect:/test/hlist.do");
+		
+		return mav;
+	}//updateProc() end
+	
+	//질문 삭제
+	@RequestMapping(value="/test/hdelete.do", method=RequestMethod.GET)
+	public ModelAndView deleteProc(int htest_no) {
+		ModelAndView mav = new ModelAndView();
+		
+		int cnt = dao.delete(htest_no);
+		
+		if(cnt==0) {
+			System.out.println("htest 질문 삭제 실패");
+		}
+		
+		mav.setViewName("redirect:/test/hlist.do");
+		
+		return mav;
+		
+	}//deleteProc() end
 }//class end
--- final06/src/main/java/kr/co/itwill/hyangtest/HyangtestDTO.java
+++ final06/src/main/java/kr/co/itwill/hyangtest/HyangtestDTO.java
@@ -2,4 +2,38 @@ package kr.co.itwill.hyangtest;
 
 public class HyangtestDTO {
 
-}
+	private int htest_no;
+	private String htest_name;
+	private int htest_code;
+	
+	public HyangtestDTO() {}
+
+	public int getHtest_no() {
+		return htest_no;
+	}
+
+	public void setHtest_no(int htest_no) {
+		this.htest_no = htest_no;
+	}
+
+	public String getHtest_name() {
+		return htest_name;
+	}
+
+	public void setHtest_name(String htest_name) {
+		this.htest_name = htest_name;
+	}
+
+	public int getHtest_code() {
+		return htest_code;
+	}
+
+	public void setHtest_code(int htest_code) {
+		this.htest_code = htest_code;
+	}
+
+	@Override
+	public String toString() {
+		return "HyangtestDTO [htest_no=" + htest_no + ", htest_name=" + htest_name + ", htest_code=" + htest_code + "]";
+	}
+}//class end
