ca0199c63887359d97a8654b9941dae655bf3825
rlaclgd <rlaclgd@naver.com>
2023-06-27 11:41:50 +0900

    [Docs] perInsert 시 p_id값 불러오도록 수정

--- final06/src/main/java/kr/co/itwill/cart/CartCont.java
+++ final06/src/main/java/kr/co/itwill/cart/CartCont.java
@@ -1,11 +1,13 @@
 package kr.co.itwill.cart;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import kr.co.itwill.member.MemberDTO;
@@ -24,15 +26,20 @@ public class CartCont {
 	public String perInsert(@ModelAttribute CartDTO dto, HttpSession session) {
 		
 		// 로그인 성공 했을 때 로그인 정보는 세션에 올려진 상태
+		// 로그인 한 p_id 가져오기
 		Object obj = session.getAttribute("member_dto");
 	    MemberDTO mDto = (MemberDTO) obj;
-     	
+	   // System.out.println(mDto);
       	
       	if(mDto==null) {
       		return "redirect:/member/login.do";  		
-      	}else { 		
+      	}else { 
+      		String p_id = mDto.getP_id(); // mDto에서 p_id값 가져옴	 
+      		
+      		dto.setP_id(p_id); // dto에 p_id 설정
+      		
       		dao.perInsert(dto);
-      		//pid 값 안넘어옴! 이거부터 하기
+      		//p_id 값 안넘어옴! 이거부터 하기
       		return "/cart/list";	
       	}
 	
